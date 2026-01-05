b5a4a19347c07d2029e8289e603095c88ff843bb
aesareetcuu6jllk <amalgurnet19@gmail.com>
2026-01-05 21:13:16 +0300

    Update test.py

--- hellas/test.py
+++ hellas/test.py
@@ -1,1 +1,7 @@
-ttt
+from main import hellas  # استدعاء المحرك الأساسي من الملف الرئيسي
+from telethon import events
+
+@hellas.on(events.NewMessage(outgoing=True, pattern=r"\.تجربة"))
+async def check_hellas(event):
+    """أمر بسيط للتأكد من أن النظام يعمل بنجاح"""
+    await event.edit("**✅ نظام hellas يعمل بنجاح!**\n\n**᯽︙ الحساب الآن متصل بالكامل وبإمكانك استخدام كافة الإضافات.**")
