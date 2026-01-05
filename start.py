786c4ff64d053b1d173823a19b91ae7b0f16c2f0
aesareetcuu6jllk <amalgurnet19@gmail.com>
2026-01-05 23:46:53 +0300

    Add new command for developer info

--- start.py
+++ start.py
@@ -6,3 +6,7 @@ from telethon import events
 @hellas.on(events.NewMessage(outgoing=True, pattern=r"^\.فحص$"))
 async def check(event):
     await event.edit("**🤖 النظام يعمل ومحدث فورا!**")
+
+@hellas.on(events.NewMessage(outgoing=True, pattern=r"^\.المطور$"))
+async def check(event):
+    await event.edit("**🤖 المطور هيلاس: @t00t0**")
