22e822e3ea059ca4ba131eca0f5eeed731fa5759
aesareetcuu6jllk <amalgurnet19@gmail.com>
2026-01-05 23:43:10 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,8 @@
+# start.py
+from main import hellas
+from telethon import events
+
+# نقوم بحذف الهاندلر القديم قبل إضافة الجديد لتجنب تكرار الاستجابة
+@hellas.on(events.NewMessage(outgoing=True, pattern=r"^\.فحص$"))
+async def check(event):
+    await event.edit("**🤖 النظام يعمل ومحدث فورا!**")
